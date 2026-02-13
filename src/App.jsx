9d837211a9050fe21e88dc00b95e00eb892b239d
Amy Johns <woobat@gmail.com>
2026-02-12 20:42:47 -0500

    props

--- src/App.jsx
+++ src/App.jsx
@@ -8,50 +8,56 @@ const welcome = {
   title: 'General Kenobi',
 };
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://react.dev',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-   title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abromov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1, 
-  },
-  
-];
 
-const App = () => 
-   (
+const App = () => {
+   
+    const stories = [
+    { 
+      title: 'React',
+      url: 'https://react.dev',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+    title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abromov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1, 
+   },
+  ];
+  return(
       <div>
         
         <h2>{welcome.greeting},{welcome.title}</h2>
-        <List />
+        <List list={stories} />
         <Search />
       </div>
   );
+};
 
 
-const List =()=> (
+const List =(props)=> (
   <ul>
-        {list.map((item)=>(
-          <li key = {item.objectID}>
-            <span><a href={item.url}>{item.title}</a></span>
-            <span> by {item.author}. </span>
-            <span>{item.num_comments} comments. </span>
-            <span>{item.points} points. </span>
-          </li>
+        {props.list.map((item)=>(
+          <Item key={item.objectID} item={item} />
         ))}
         </ul>
  )
 
+ const Item = (props) =>(
+          <li>
+            <span><a href={props.item.url}>{props.item.title}</a></span>
+            <span> by {props.item.author}. </span>
+            <span>{props.item.num_comments} comments. </span>
+            <span>{props.item.points} points. </span>
+          </li>
+
+
+ )
 
 const Search=()=>{
   const handleChange = (event)=> {
